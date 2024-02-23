6e8a0390b7a972b28e1f606eb4ff1be9d76018ab
TOOm <97885350+TomVoituret@users.noreply.github.com>
2024-02-23 15:13:04 +0100

    creation de l'appli java spring boot

---
+++ src/test/java/java/tyg/tradinggame/tradinggame/TradingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package java.tyg.tradinggame.tradinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
